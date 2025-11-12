5d12da5078d3ab35f1cf1aeb9781040b22176cf2
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2025-11-12 15:57:25 +0100

    Print info that poetry was added to path

--- stress-l3/worker/install-poetry.sh
+++ stress-l3/worker/install-poetry.sh
@@ -6,8 +6,9 @@ echo "Installing Poetry..."
 curl -sSL https://install.python-poetry.org | python3 -
 
 # Add poetry to PATH in bashrc
-if ! grep -q '\.local/bin' "$HOME/.bashrc"; then
+if ! grep -q 'export PATH=.*\.local/bin' "$HOME/.bashrc"; then
     echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc"
+    echo "Poetry PATH ( $HOME/.local/bin ) added to $HOME/.bashrc"
 fi
 
 export PATH="$HOME/.local/bin:$PATH"
