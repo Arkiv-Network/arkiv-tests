ddf240a129a860f36b8f569e86b28b1369b700d3
scx1332 <sieciech@gmail.com>
2026-01-21 20:47:26 +0100

    Work on setup

--- init_geth.sh
+++ init_geth.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-set -x
+set -e # Exit on error
+set -x # Print commands
 
-./geth --datadir "${GETH_SQLITE_DATA_DIRECTORY:-data}" init ./genesis.json
+# Default address if not provided
+ADDR="${MAIN_ACCOUNT_ADDRESS:-0x70997970C51812dc3A010C7d01b50e0d17dc79C8}"
+DATA_DIR="${GETH_SQLITE_DATA_DIRECTORY:-data}"
 
+# Use | as a delimiter to avoid confusion with address characters
+# This replaces the string "MAIN_ACCOUNT" with the actual address
+sed -i "s|MAIN_ACCOUNT|$ADDR|g" ./genesis.json
+
+# Initialize Geth
+./geth --datadir "$DATA_DIR" init ./genesis.json
