9ed69776c759721ce5144d476bcd39938f3fd90e
scx1332 <sieciech@gmail.com>
2026-02-16 21:46:15 +0100

    Added bridging geth metrics

--- metrics-bridge.py
+++ metrics-bridge.py
@@ -2,18 +2,18 @@ import os
 import requests
 import time
 
-GETH_METRICS_URL = os.getenv("METRICS_URL")
-PUSHGATEWAY_URL = os.getenv("PUSHGATEWAY_URL")
+METRICS_URL = os.getenv("METRICS_URL")
+PUSH_GATEWAY_URL = os.getenv("PUSH_GATEWAY_URL")
 INTERVAL = int(os.getenv("PUSH_INTERVAL", 1))
 
 def push_metrics():
     try:
         # 1. Scrape Geth
-        response = requests.get(GETH_METRICS_URL)
+        response = requests.get(METRICS_URL)
         response.raise_for_status()
 
         # 2. Push to Gateway
-        push_res = requests.post(PUSHGATEWAY_URL, data=response.text)
+        push_res = requests.post(PUSH_GATEWAY_URL, data=response.text)
         push_res.raise_for_status()
         print(f"Metrics pushed successfully at {time.ctime()}")
     except Exception as e:
